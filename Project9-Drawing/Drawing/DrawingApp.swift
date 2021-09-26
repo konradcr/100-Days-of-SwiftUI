1e40120ab2aacd6311952dd3af36ac40ee8de096
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-26 18:47:27 +0200

    Project 9 - Drawing

---
+++ Project9-Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Konrad Cureau on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
