22fff837ad86ea330cbb686f413f22ebb76da109
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-26 18:28:10 +0200

    Project 3 - ViewsAndModifiers

---
+++ Project3-ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Konrad Cureau on 10/04/2021.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        GridStack(rows: 4, columns: 2) { row, col in
+            Image(systemName: "\(row * 2 + col).circle")
+            Text("R\(row) C\(col)")
+                .titleStyle()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
