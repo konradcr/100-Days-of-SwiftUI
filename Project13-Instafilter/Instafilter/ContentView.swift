a3503557e6333b942c469f02db3ac2bb574fb734
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-26 19:08:47 +0200

    Project 13 - Instafilter

---
+++ Project13-Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,196 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Konrad Cureau on 01/07/2021.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
+    
+    @State private var showingFilterSheet = false
+    @State private var showingImagePicker = false
+    @State private var showingImageAlert = false
+    @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
+    
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
+    var hasIntensity: Bool {
+            currentFilter.inputKeys.contains(kCIInputIntensityKey)
+    }
+
+    var hasRadius: Bool {
+        currentFilter.inputKeys.contains(kCIInputRadiusKey)
+    }
+
+    var hasScale: Bool {
+        currentFilter.inputKeys.contains(kCIInputScaleKey)
+    }
+    
+    var body: some View {
+        let intensity = Binding<Double>(
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+        let radius = Binding<Double>(
+            get: {
+                self.filterRadius
+            },
+            set: {
+                self.filterRadius = $0
+                self.applyProcessing()
+            }
+        )
+        let scale = Binding<Double>(
+            get: {
+                self.filterScale
+            },
+            set: {
+                self.filterScale = $0
+                self.applyProcessing()
+            }
+        )
+        
+        return NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    self.showingImagePicker = true
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: intensity)
+                }.padding(.vertical)
+                .disabled(hasIntensity == false)
+                
+                HStack {
+                    Text("Radius")
+                    Slider(value: radius)
+                }.padding(.vertical)
+                .disabled(hasRadius == false)
+                
+                HStack {
+                    Text("Scale")
+                    Slider(value: scale)
+                }.padding(.vertical)
+                .disabled(hasScale == false)
+
+                HStack {
+                    Button("\((CIFilter.localizedName(forFilterName: self.currentFilter.name) ?? "Select a Filter"))" ) {
+                        self.showingFilterSheet = true
+                    }
+
+                    Spacer()
+
+                    Button("Save") {
+                        guard let processedImage = self.processedImage else {
+                            self.showingImageAlert = true
+                            return }
+
+                        let imageSaver = ImageSaver()
+
+                        imageSaver.successHandler = {
+                            print("Success!")
+                        }
+
+                        imageSaver.errorHandler = {
+                            print("Oops: \($0.localizedDescription)")
+                        }
+
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
+                    }
+                    .alert(isPresented: $showingImageAlert) {
+                                Alert(title: Text("Important message"), message: Text("No picture selected"), dismissButton: .default(Text("Got it!")))
+                            }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
+            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: self.$inputImage)
+            }
+            .actionSheet(isPresented: $showingFilterSheet) {
+                ActionSheet(title: Text("Select a filter"), buttons: [
+                    .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
+                    .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
+                    .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
+                    .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate()) },
+                    .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone()) },
+                    .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask()) },
+                    .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
+                    .cancel()
+                ])
+            }
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        if hasIntensity {
+                    print("set intensity \(filterIntensity)")
+                    currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        if hasRadius {
+            print("set radius \(filterRadius * 200)")
+            currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey)
+        }
+        if hasScale {
+            print("set scale \(filterScale * 10)")
+            currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+            processedImage = uiImage
+        }
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project13-Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Konrad Cureau on 01/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
