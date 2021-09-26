b6b9ecfd50ed49ad85312ffd1c8d2b1f682a1792
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-26 19:03:00 +0200

    Project 12 - CoreDataProject

---
+++ Project12-CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Konrad Cureau on 21/06/2021.
+//
+
+import CoreData
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @State private var lastNameFilter = "A"
+    
+    var body: some View {
+        VStack {
+            FilteredList(filter: lastNameFilter)
+
+            Button("Add Examples") {
+                let taylor = Singer(context: self.moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
+
+                let ed = Singer(context: self.moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
+
+                let adele = Singer(context: self.moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
+
+                try? self.moc.save()
+            }
+
+            Button("Show A") {
+                self.lastNameFilter = "A"
+            }
+
+            Button("Show S") {
+                self.lastNameFilter = "S"
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project12-CoreDataProject/CoreDataProject/FilteredList.swift
@@ -0,0 +1,22 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Konrad Cureau on 25/06/2021.
+//
+
+import SwiftUI
+
+struct FilteredList: View {
+    var fetchRequest: FetchRequest<Singer>
+    init(filter: String) {
+        fetchRequest = FetchRequest<Singer>(entity: Singer.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Singer.lastName, ascending: true),NSSortDescriptor(keyPath: \Singer.firstName, ascending: true)], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    }
+    
+    var body: some View {
+        List(fetchRequest.wrappedValue, id: \.self) { singer in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
+    }
+}
+
