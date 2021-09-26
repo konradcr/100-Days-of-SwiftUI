2309e44dd029f236042afe05acb16817a08aa267
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-26 19:15:27 +0200

    Project 18 - LayoutAndGeometry

---
+++ Project18-LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Konrad Cureau on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
