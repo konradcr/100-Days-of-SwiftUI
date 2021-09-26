d250f4c65639b68f34364b122aa8b9fc1cb81e74
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-26 18:40:31 +0200

    Project 6 - Animations

---
+++ Project6-Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Konrad Cureau on 13/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
